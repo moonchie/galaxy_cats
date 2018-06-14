2a59033d3cd1fdd928364789c33fcfe65f1a4dbe
shanx2 <shanshan.cao@live.nl>
2018-06-14 12:29:43 +0200

    new home screen with upgrated UI

--- js/states/Home.js
+++ js/states/Home.js
@@ -1,4 +1,8 @@
 var galaxyCats = galaxyCats || {};
+var filter;
+var sprite;
+var bmd;
+var text;
 
 // HOME SCREEN
 galaxyCats.HomeState = {
@@ -14,28 +18,43 @@ galaxyCats.HomeState = {
 
     preload: function() {
         //assets we'll use in the loading screen
-        this.load.image("galaxy", "assets/images/galaxy3.jpg");
-        this.load.image("button", "assets/images/button_classic.png");
-        //this.load.bitmapFont('myfont', 'assets/images/font.png', 'assets/images/font.fnt');
+        this.load.image("galaxy", "assets/images/galaxy_pretty.png");
+        this.load.image("button", "assets/images/yes_button.png");
+        this.load.image("cat", "assets/images/catgalaxy.png");
 
         //google font
-        this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
+        this.load.script('Audiowide', 'https://fonts.googleapis.com/css?family=Audiowide');
     },
     create: function() {
         var background = this.game.add.sprite(0,0,"galaxy");
-        var button = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY+150, "button");
-        button.anchor.setTo(0.5);
-        button.scale.setTo(0.3);
-        var style = { font: "40px Bauhaus93", fill: "#ff0044", align: "center" };
-        var text = this.add.text(this.world.centerX, this.world.centerY-150, "GALAXY CATS", style);
+        /*var catBackground = this.game.add.sprite(this.world.centerX, this.world.centerY,"cat");
+        catBackground.scale.setTo(0.3);
+        catBackground.anchor.setTo(0.5);
+        */
+
+
+        //var style = {font: "60px Revalia", fill: "#ff0044", align: "center" };
+        var text = this.add.text(this.world.centerX, this.world.centerY-100, "GALAXY \n CATS",{ font: "30px Audiowide"} );
         text.anchor.set(0.5);
+        text.fontSize = 45;
+        grd = text.context.createLinearGradient(0, 0, 0, text.canvas.height);
+        grd.addColorStop(0, '#fffacd');
+        grd.addColorStop(1, '#e9967a');
+        text.stroke = '#000000';
+        text.strokeThickness = 2;
+        text.setShadow(3, 3, '#adadb2', 5);
+        text.fill = grd;
 
 
 
+        //-------BUTTON------------
+        var button = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY+200, "button");
+        button.anchor.setTo(0.5);
+        button.scale.setTo(0.5);
         button.inputEnabled = true;
         button.events.onInputDown.add(function(){
             this.state.start('Boot');
         },this)
+    },
 
-    }
   };
